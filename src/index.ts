863ea7651c68859aeaedd8f59c065ac2c0a4d775
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-14 13:52:00 +0300

    Refactor server setup to integrate Better Auth and update listening port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import express from "express";
 import { app } from "./app";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
+const port = 8000;
 
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 
-app.listen(3000, () => {
-  console.log(`Server running on port 3000`);
+// Mount express json middleware after Better Auth handler
+// or only apply it to routes that don't interact with Better Auth
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Better Auth app listening on port ${port}`);
 });
