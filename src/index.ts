048a227e981c50d7f553a2088630752dd33739e4
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-14 18:11:26 +0300

    Remove outdated comments in index.ts for clarity

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import { app } from "./app";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 
+
 const port = 8000;
 
 app.all("/api/auth/{*any}", toNodeHandler(auth));
 
-// Mount express json middleware after Better Auth handler
-// or only apply it to routes that don't interact with Better Auth
+
 app.use(express.json());
 
 app.listen(port, () => {
