f6566a52c01ba3acc486f615ed8b4d814ea3570a
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-15 20:30:57 +0300

    Refactor index.ts for improved code organization and readability

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
-import express from "express";
-import { app } from "./app";
-import { toNodeHandler } from "better-auth/node";
-import { auth } from "./lib/auth";
+import express from 'express';
+import { app } from './app';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth';
+import { userRouter } from './router/userRouter';
+import morgan from 'morgan';
+
+
 
 
 const port = 8000;
 
-app.all("/api/auth/{*any}", toNodeHandler(auth));
-
-
 app.use(express.json());
+app.use(morgan('dev'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', userRouter);
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.listen(port, () => {
   console.log(`Better Auth app listening on port ${port}`);
 });
